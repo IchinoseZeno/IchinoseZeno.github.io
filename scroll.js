d7a11fbe8b2adf8d46ade52b8bcf9f0faa5074df
IchinoseZeno <50504674+IchinoseZeno@users.noreply.github.com>
2019-05-24 09:27:53 +0900

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,1 +1,14 @@
-
+function　scrollFunction() {
+    var this_y = window.pageYOffset;
+    if (this_y > 240) {
+        document.getElementById("send").classList.add("js-active");
+    } else {
+        document.getElementById("send").classList.remove("js-active");
+    }
+}
+window.onload = function() {
+    scrollFunction();
+}
+window.onscroll = function() {
+    scrollFunction();
+}
