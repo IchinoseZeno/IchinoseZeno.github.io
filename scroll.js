11aafe8b98cbdcb194a14d307f9ca68dcd371fe8
IchinoseZeno <50504674+IchinoseZeno@users.noreply.github.com>
2019-05-24 10:23:41 +0900

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,10 +1,12 @@
 function　scrollFunction() {
     var this_y = window.pageYOffset;
     if (this_y > 240) {
-        document.getElementsByClassName(".leftToCenter-header .net-image").classList.add("js-active");
+        document.getElementById("send-header").classList.add("js-active");
+        document.getElementById("send-header-net").classList.add("js-active");
     } else {
-        document.getElementsByClassName(".leftToCenter-header .net-image").classList.remove("js-active");
-    }
+        document.getElementById("send-header").classList.remove("js-active");
+        document.getElementById("send-header-net").classList.remove("js-active");
+      }
 }
 window.onload = function() {
     scrollFunction();
